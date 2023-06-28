17d35fdab6c4f087c9eca635d81ae443e64ee23b
Fredrik Fornwall <fredrik@fornwall.net>
2023-06-28 17:06:42 +0200

    Add more rules

--- src/rules.rs
+++ src/rules.rs
@@ -31,12 +31,66 @@ impl Rule {
     }
 }
 
-pub static RULES: [Rule; 9] = [
+pub static RULES: [Rule; 18] = [
     Rule {
         born: 0b1000,
         survives: 0b1100,
         name: "Conway's Life",
         initial_density: 12,
+    },
+    Rule {
+        born: 0b0_1011_1000,
+        survives: 0b1_0111_0000,
+        name: "Gems",
+        initial_density: 15,
+    },
+    Rule {
+        born: 0b0_0000_0010,
+        survives: 0b0_0000_0010,
+        name: "Gnarl",
+        initial_density: 15,
+    },
+    Rule {
+        born: 0b1_1100_1000,
+        survives: 0b1_1101_1000,
+        name: "Day & Night",
+        initial_density: 50,
+    },
+    Rule {
+        born: 0b1_1110_1000,
+        survives: 0b1_1110_0000,
+        name: "Diamoeba	",
+        initial_density: 48,
+    },
+    Rule {
+        born: 0b0_1000_1000,
+        survives: 0b0_0000_1100,
+        name: "DryLife",
+        initial_density: 12,
+    },
+    Rule {
+        born: 0b1_1110_0100,
+        survives: 0b1_1110_0000,
+        name: "Iceballs	",
+        initial_density: 1,
+    },
+    Rule {
+        born: 0b0_0010_1000,
+        survives: 0b1_1011_1100,
+        name: "Land Rush",
+        initial_density: 4,
+    },
+    Rule {
+        born: 0b0_0100_1000,
+        survives: 0b1_1011_1100,
+        name: "Land Rush 2",
+        initial_density: 4,
+    },
+    Rule {
+        born: 0b0_0000_0100,
+        survives: 0b0_0000_0000,
+        name: "Live Free or Die	",
+        initial_density: 1,
     },
     Rule {
         born: 0b0_0000_1000,
@@ -51,28 +105,28 @@ pub static RULES: [Rule; 9] = [
         initial_density: 3,
     },
     Rule {
+        born: 0b1_0000_1000,
+        survives: 0b0_0000_1100,
+        name: "Pedestrian Life",
+        initial_density: 10,
+    },
+    Rule {
         born: 0b0_1010_1010,
         survives: 0b0_1010_1010,
         name: "Replicator",
-        initial_density: 8,
+        initial_density: 1,
     },
     Rule {
-        born: 0b1_1100_1000,
-        survives: 0b1_1101_1000,
-        name: "Day & Night",
-        initial_density: 30,
+        born: 0b0_0001_1100,
+        survives: 0b0_0000_0000,
+        name: "Serviettes",
+        initial_density: 1,
     },
     Rule {
-        born: 0b0_0010_1000,
-        survives: 0b1_1011_1100,
-        name: "Land Rush",
-        initial_density: 4,
-    },
-    Rule {
-        born: 0b0_0100_1000,
-        survives: 0b1_1011_1100,
-        name: "Land Rush 2",
-        initial_density: 4,
+        born: 0b0_0000_1000,
+        survives: 0b0_1000_1110,
+        name: "SnowLife",
+        initial_density: 3,
     },
     Rule {
         born: 0b1_1100_1000,
